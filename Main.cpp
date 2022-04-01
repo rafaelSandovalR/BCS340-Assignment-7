462172ce3f2ad3a1b4d143415fd0e46d81dd6aa9
gmartsm <84422381+gmartsm@users.noreply.github.com>
2022-04-01 11:09:55 -0400

    Update Main.cpp
    Added Selection sort and Merge sort function, will be updating to make code more readable...

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,20 @@
 #include <ctime>
 
 // Selection Sort - Gary
-
+void selectionSort(int a[] , int size) {
+  int outer, inner, min;
+  for (outer = 0; outer < size - 1; outer++) {
+    min = outer;
+    for (inner = outer + 1; inner < size ; inner++) {
+      if (a[inner] < a[min]) {
+        min = inner;
+    }
+  }
+  int temp = a[outer];
+  a[outer] = a[min];
+  a[min] = temp;
+  }
+}
 // Insertion Sort - Brian
 
 // Bubble Sort - Raf
@@ -15,6 +28,76 @@ void bubbleSort(int arr[]) {
 }
 
 // Merge Sort - Gary
+void merge(int arr[], int l, int m, int r) 
+{ 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 =  r - m; 
+  
+    /* create temp arrays */
+    int L[n1], R[n2]; 
+  
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1+ j]; 
+  
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray 
+    j = 0; // Initial index of second subarray 
+    k = l; // Initial index of merged subarray 
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < n1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < n2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+  
+/* l is for left index and r is right index of the 
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r) 
+{ 
+    if (l < r) 
+    { 
+        // Same as (l+r)/2, but avoids overflow for 
+        // large l and h 
+        int m = l+(r-l)/2; 
+  
+        // Sort first and second halves 
+        mergeSort(arr, l, m); 
+        mergeSort(arr, m+1, r); 
+  
+        merge(arr, l, m, r); 
+    } 
+}
 
 // Quick Sort - Brian
 
@@ -100,4 +183,3 @@ int main() {
     }
 
 }
-    
